defc065f45c686d595ddfeaa1bb6bd56ea5d998a
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-04-20 13:01:12 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,7 +13,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 # variables
 API_ID = "24665357" #config("API_ID", default=None, cast=int)
-API_HASH = "beb7e4b83ada668fa85f9a9b56338f1d" 3config("API_HASH", default=None)
+API_HASH = "beb7e4b83ada668fa85f9a9b56338f1d" #config("API_HASH", default=None)
 BOT_TOKEN = "6700540841:AAEzEG75XEQXqfTGmIvy136zVclAUBxQKOI" #config("BOT_TOKEN", default=None)
 SESSION = "1BVtsOJ4Buzj-vIEfWXZ6GAdPuF4VvAWXd-t6FXOgOBKXAHKUak__gaSv3TYGotZ--GOILmpaCbdebjZlkfFR3Y64fkcnF6CcKfaCzGrQNzUaFhk6zlAI5qbeuSzD_5_pgy3XUxQ2oE9auLsbKs_na3OQYQzrnOd5nYvFRpuHdaWEc00-kCcO59ZTizmCSHt4-5Su3NC9z1j90acRKltNOiimRSaZtKXyO7YivjROypj9L8LiUK0UBGMWblfhXe1e4okfSoKqbJO4g49B6Bpcvdsq9PZUZOkjYkp7qE2beCXsubpWf_LlN9I8pG9vBKViOSha_KFNOW7MESwHae0NLZdvcoDqCB8=" #config("SESSION", default=None)
 FORCESUB = "-1002137417670" #config("FORCESUB", default=None)
